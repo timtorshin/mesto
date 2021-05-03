13f768ef8061c664abafb9b098fcad3b0abdd780
Timofei Torshin <timtorshin@gmail.com>
2021-05-03 19:00:53 +0300

    refactor: modified popups

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,6 @@ const popupFormAdd = document.querySelector('.popup__form_type_add');
 const popupOpenAdd = document.querySelector('.profile__add-button');
 const popupCloseAdd = document.querySelector('.popup__close-button_type_add');
 
-const buttonAdd = document.querySelector('.popup__submit-button_type_add');
 const inputTitle = document.querySelector('#element-title');
 const inputLink = document.querySelector('#element-link');
 const elementsContainer = document.querySelector('.elements');
@@ -51,78 +50,54 @@ const popupCloseImage = document.querySelector('.popup__close-button_type_image'
 const popupPhoto = document.querySelector('.popup__image');
 const popupFigcaption = document.querySelector('.popup__figcaption');
 
-//Функция открытия всплывающего окна редактирования профиля
-function openPopupEdit() {
-  popupEdit.classList.add('popup_opened');
+//Функция открытия всплывающего окна
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+//Функция закрытия всплывающего окна
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//Открытие окна редактирования профиля
+popupOpenEdit.addEventListener('click', function() {
+  openPopup(popupEdit);
 
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
-}
+});
 
-//Функция закрытия всплывающего окна редактирования профиля
-function closePopupEdit() {
-  popupEdit.classList.remove('popup_opened');
-}
+//Закрытие окна редактирования профиля
+popupCloseEdit.addEventListener('click', function() {
+  closePopup(popupEdit);
+});
 
 //Функция отправки формы редактирования профиля
-function formSubmitHandlerEdit(evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
 
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
 
-  closePopupEdit();
+  closePopup(popupEdit);
 }
 
-popupOpenEdit.addEventListener('click', openPopupEdit);
-popupCloseEdit.addEventListener('click', closePopupEdit);
-popupFormEdit.addEventListener('submit', formSubmitHandlerEdit);
+//Отправка формы редактирования профиля 
+popupFormEdit.addEventListener('submit', handleEditFormSubmit);
 
-function createElement(item) {
-  const newElement = elementTemplate.content.querySelector('.element').cloneNode(true);
-  const elementTitle = newElement.querySelector('.element__title');
-  const elementLink = newElement.querySelector('.element__image');
-  const likeButton = newElement.querySelector('.element__like-button');
-  const deleteButton = newElement.querySelector('.element__delete-button');
-
-  elementTitle.textContent = item.name;
-  elementLink.src = item.link;
-
-  likeButton.addEventListener('click', function(evt) {
-    evt.target.classList.toggle('element__like-button_active');
-  });
-
-  deleteButton.addEventListener('click', function(evt) {
-    evt.target.closest('.element').remove();
-  });
-
-  elementLink.addEventListener('click', function() {
-    popupPhoto.src = item.link;
-    popupFigcaption.textContent = item.name;
-
-    openPopupImage();
-  });
-
-  return newElement;
-}
-
-initialElements.forEach(function(currentItem) {
-  const newItem = createElement(currentItem);
-  elementsContainer.append(newItem);
+//Открытие окна добавления карточек
+popupOpenAdd.addEventListener('click', function() {
+  openPopup(popupAdd);
 });
 
-//Функция открытия всплывающего окна добавления карточек
-function openPopupAdd() {
-  popupAdd.classList.add('popup_opened');
-}
-
-//Функция закрытия всплывающего окна добавления карточек
-function closePopupAdd() {
-  popupAdd.classList.remove('popup_opened');
-}
+//Закрытие окна добавления карточек
+popupCloseAdd.addEventListener('click', function() {
+  closePopup(popupAdd);
+});
 
 //Функция отправки формы добавления карточек
-function formSubmitHandlerAdd(evt) {
+function handleAddFormSubmit(evt) {
   evt.preventDefault();
 
   const oneMoreItem = {
@@ -134,21 +109,48 @@ function formSubmitHandlerAdd(evt) {
 
   elementsContainer.prepend(createElement(oneMoreItem));
 
-  closePopupAdd();
+  closePopup(popupAdd);
 }
 
-popupOpenAdd.addEventListener('click', openPopupAdd);
-popupCloseAdd.addEventListener('click', closePopupAdd);
-popupFormAdd.addEventListener('submit', formSubmitHandlerAdd);
+//Отправка формы добавления карточек
+popupFormAdd.addEventListener('submit', handleAddFormSubmit);
 
-//Функция открытия просмотра фотографии
-function openPopupImage() {
-  popupOpenImage.classList.add('popup_opened');
+//Закрытие просмотра фотографии
+popupCloseImage.addEventListener('click', function() {
+  closePopup(popupOpenImage);
+});
+
+function createElement(item) {
+  const newElement = elementTemplate.content.querySelector('.element').cloneNode(true);
+  const elementTitle = newElement.querySelector('.element__title');
+  const elementLink = newElement.querySelector('.element__image');
+  const likeButton = newElement.querySelector('.element__like-button');
+  const deleteButton = newElement.querySelector('.element__delete-button');
+
+  elementTitle.textContent = item.name;
+  elementLink.src = item.link;
+  elementLink.alt = 'Фотография ' + item.name;
+
+  likeButton.addEventListener('click', function(evt) {
+    evt.target.classList.toggle('element__like-button_active');
+  });
+
+  deleteButton.addEventListener('click', function(evt) {
+    evt.target.closest('.element').remove();
+  });
+
+  elementLink.addEventListener('click', function() {
+    popupPhoto.src = item.link;
+    popupPhoto.alt = 'Фотография ' + item.name;
+    popupFigcaption.textContent = item.name;
+
+    openPopup(popupOpenImage);
+  });
+
+  return newElement;
 }
 
-//Функция закрытия просмотра фотографии
-function closePopupImage() {
-  popupOpenImage.classList.remove('popup_opened');
-}
-
-popupCloseImage.addEventListener('click', closePopupImage);
+initialElements.forEach(function(currentItem) {
+  const newItem = createElement(currentItem);
+  elementsContainer.append(newItem);
+});
